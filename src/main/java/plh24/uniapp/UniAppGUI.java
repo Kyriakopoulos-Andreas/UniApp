7fd839089973ec1c36dfda785e0edcd3b91afc87
stetsip <stetsip@gmail.com>
2025-02-25 13:35:35 +0200

    -add

--- src/main/java/plh24/uniapp/UniAppGUI.java
+++ src/main/java/plh24/uniapp/UniAppGUI.java
@@ -12,13 +12,29 @@ import okhttp3.Request;
 import okhttp3.Response;
 import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
+//import com.itextpdf.kernel.pdf.PdfDocument;
+//import com.itextpdf.kernel.pdf.PdfWriter;
+//import com.itextpdf.layout.Document;
+//import com.itextpdf.layout.element.Paragraph;
+//import com.itextpdf.layout.element.Table;
+//import com.itextpdf.kernel.font.PdfFontFactory;
+//import com.itextpdf.kernel.font.PdfFont;
+
+import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.colors.ColorConstants;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
-import com.itextpdf.kernel.font.PdfFontFactory;
-import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.UnitValue;
+
+
+
 
 /**
  *
@@ -139,7 +155,78 @@ public class UniAppGUI extends JFrame {
         JTable statsTable = new JTable(statsModel);
         JOptionPane.showMessageDialog(this, new JScrollPane(statsTable), "Στατιστικά Πανεπιστημίων", JOptionPane.INFORMATION_MESSAGE);
     }
+    
+private void exportStatisticsToPDF() {
+    UniversityDAO dao = UniversityDAO.getInstance();
+    List<University> popularUniversities = dao.getPopularUniversities();
 
+    if (popularUniversities.isEmpty()) {
+        JOptionPane.showMessageDialog(this, "Δεν υπάρχουν διαθέσιμα στατιστικά για εκτύπωση.");
+        return;
+    }
+
+    try {
+        PdfWriter writer = new PdfWriter("statistics.pdf");
+        PdfDocument pdf = new PdfDocument(writer);
+        Document document = new Document(pdf);
+
+        // Φόρτωση γραμματοσειράς που υποστηρίζει ελληνικούς χαρακτήρες
+        PdfFont font = PdfFontFactory.createFont("resources/fonts/FreeSans.ttf", PdfEncodings.IDENTITY_H, true);
+        document.setFont(font);
+
+        // Τίτλος
+        Paragraph title = new Paragraph("Στατιστικά Δημοφιλέστερων Αναζητήσεων")
+                .setFontSize(18)
+                .setBold()
+                .setTextAlignment(TextAlignment.CENTER)
+                .setMarginBottom(20);
+        document.add(title);
+
+        // Ορισμός πίνακα
+        float[] columnWidths = {1, 4, 3, 2};
+        Table table = new Table(UnitValue.createPercentArray(columnWidths))
+                .useAllAvailableWidth();
+
+        // Επικεφαλίδες με σκούρο φόντο και κεντρική στοίχιση
+        table.addHeaderCell(new Cell().add(new Paragraph("ID"))
+                .setBackgroundColor(ColorConstants.LIGHT_GRAY)
+                .setBold()
+                .setTextAlignment(TextAlignment.CENTER));
+        table.addHeaderCell(new Cell().add(new Paragraph("Όνομα Πανεπιστημίου"))
+                .setBackgroundColor(ColorConstants.LIGHT_GRAY)
+                .setBold()
+                .setTextAlignment(TextAlignment.CENTER));
+        table.addHeaderCell(new Cell().add(new Paragraph("Χώρα"))
+                .setBackgroundColor(ColorConstants.LIGHT_GRAY)
+                .setBold()
+                .setTextAlignment(TextAlignment.CENTER));
+        table.addHeaderCell(new Cell().add(new Paragraph("Προβολές"))
+                .setBackgroundColor(ColorConstants.LIGHT_GRAY)
+                .setBold()
+                .setTextAlignment(TextAlignment.CENTER));
+
+        // Προσθήκη δεδομένων
+        for (University uni : popularUniversities) {
+            table.addCell(new Cell().add(new Paragraph(String.valueOf(uni.getId())))
+                    .setTextAlignment(TextAlignment.CENTER));
+            table.addCell(new Cell().add(new Paragraph(uni.getName()))
+                    .setTextAlignment(TextAlignment.LEFT));
+            table.addCell(new Cell().add(new Paragraph(uni.getCountry()))
+                    .setTextAlignment(TextAlignment.LEFT));
+            table.addCell(new Cell().add(new Paragraph(String.valueOf(uni.getViewCount())))
+                    .setTextAlignment(TextAlignment.CENTER));
+        }
+
+        document.add(table);
+        document.close();
+        JOptionPane.showMessageDialog(this, "Το αρχείο statistics.pdf δημιουργήθηκε με επιτυχία!");
+    } catch (Exception e) {
+        e.printStackTrace();
+        JOptionPane.showMessageDialog(this, "❌ Σφάλμα κατά τη δημιουργία του PDF.", "Σφάλμα", JOptionPane.ERROR_MESSAGE);
+    }
+}
+    
+/**
     private void exportStatisticsToPDF() {
         UniversityDAO dao = UniversityDAO.getInstance();
         List<University> popularUniversities = dao.getPopularUniversities();
@@ -176,7 +263,7 @@ public class UniAppGUI extends JFrame {
             JOptionPane.showMessageDialog(this, "❌ Σφάλμα κατά τη δημιουργία του PDF.", "Σφάλμα", JOptionPane.ERROR_MESSAGE);
         }
     }
-
+*/
     private void refreshDataFromWebService() {
         SwingWorker<Void, Integer> worker = new SwingWorker<>() {
             @Override
